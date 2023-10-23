44b8718ec9fa021941159bbd314d85ab49573b32
JohnMichaelMWong <johnmichaelmwong0411@gmail.com>
2023-10-23 17:00:08 +0800

    update contact

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -3,14 +3,13 @@ const Contact = () => {
     return (
         <div id="Contact" className="Contact-container">
             <div class='contactContent'>
-                    <h1>Contact Us</h1>
+                    <h1>Contact <span>Us</span></h1>
                     <div class='contactInfo'>
-                        <p>PC Hill, Rosary Heights 1, Mabini St, Cotabato City, Lalawigan ng Maguindanao</p>
+                        <div>
+                        <p>PC Hill, Rosary Heights 1, Mabini St, Cotabato City</p>
                         <p>+639271234567 / 421-0101</p>
                         <p>lawyers@gmail.com</p>
-                    </div>
-                    <div class='contactForm'>
-
+                        </div>
                     </div>
             </div>       
         </div>
