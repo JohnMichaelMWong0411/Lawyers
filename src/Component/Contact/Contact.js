fbe0b2bcf62237469173d0258ceaf4cf2e16cf10
JohnMichaelMWong <johnmichaelmwong0411@gmail.com>
2023-10-25 15:39:30 +0800

    update 10252023

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -18,7 +18,18 @@ const Contact = () => {
                         <p>lawyers@gmail.com</p>
                         <h3><FontAwesomeIcon icon={faLocationDot} style={{ color: '#e5bb69' }} /><span> Address : </span></h3>
                         <p>PC Hill, Rosary Heights 1, Mabini Street, Cotabato City</p>
-                            {/* <div class='contactForm'>
+                            <iframe 
+                                title="Google Maps"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.1822253449486!2d124.24387102290233!3d7.22004478229875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32563bd7005fa563%3A0x85a2f9280fe1ddae!2sPDEA%20BARMM!5e0!3m2!1sen!2sph!4v1698118551257!5m2!1sen!2sph"
+                                width="500"
+                                height="400"
+                                style={{ border: "1px solid white" }}
+                                allowfullscreen=""
+                                loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"/>
+                        </div>
+                        <div class='contactInfoDiv'>
+                        {/* <div class='contactForm'>
                             <form action="/action_page.php">
                             <label for="name">Name</label>
                             <input type="text" id="name" name="name" placeholder="Your name.."></input>
@@ -29,18 +40,6 @@ const Contact = () => {
                             <input type="submit" value="Submit"></input>
                             </form>
                             </div> */}
-                            <iframe 
-                                title="Google Maps"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.1822253449486!2d124.24387102290233!3d7.22004478229875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32563bd7005fa563%3A0x85a2f9280fe1ddae!2sPDEA%20BARMM!5e0!3m2!1sen!2sph!4v1698118551257!5m2!1sen!2sph"
-                                width="550"
-                                height="400"
-                                style={{ border: "1px solid #e5bb69" }}
-                                allowfullscreen=""
-                                loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade"/>
-                        </div>
-                        <div class='contactInfoDiv'>
-                        
                         </div>
                     </div>
             </div>       
@@ -48,3 +47,4 @@ const Contact = () => {
     );
   }
   export default Contact;
+  
